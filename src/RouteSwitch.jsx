16a49e9a0de28000e63a52021a61ffc68a59b663
Alex Brik <115663358+AlexB528@users.noreply.github.com>
2023-11-24 16:31:35 -0500

    change from BrowserRouter to HashRouter

--- src/RouteSwitch.jsx
+++ src/RouteSwitch.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import Game from "./Game";
 import Leaderboard from "./Leaderboard";
 
 const RouteSwitch = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/Game" element={<Game />} />
         <Route path="/leaderboard" element={<Leaderboard />} />
 
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
